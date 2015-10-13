7d11309f61faabf42590f2a329e3906637961b5d
Paul Nitto <pnitto18@gmail.com>
2015-10-13 08:46:38 -0400

    Update 2015-10-13T12:46:35.958Z

--- app/scripts/main.js
+++
@@ -1,8 +0,0 @@
-
-var Router = require('router');
-
-$(document).ready(function(){
-  // prepend the contents of `app/templates/application.hbs` into `body`
-  window.appRouter = new Router();
-  Backbone.history.start();
-  });
--- app/scripts/models/collection.js
+++
@@ -1,8 +0,0 @@
-var BoardModel = require('models/board-model');
-
-var Collection = Backbone.Collection.extend({
-  model: BoardModel,
-  url: "http://tiny-lasagna-server.herokuapp.com/collections/snowboard"
-});
-
-module.exports = Collection;
